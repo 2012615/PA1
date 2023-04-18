26951970e90d28a19718cea717a14bff011d7ef2
2012615 <2536196890@qq.com>
2023-04-18 11:10:11 +0800

    完成指令的补充

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,89 +1,70 @@
 #include "common.h"
 #include "syscall.h"
-#include "fs.h"
 
-int sys_none() {
-    return 1;
+extern char _end;
+extern ssize_t fs_read(int fd, void *buf, size_t len);
+extern ssize_t fs_write(int fd, const void *buf, size_t len);
+extern int fs_open(const char *pathname, int flags, int mode);
+extern off_t fs_lseek(int fd, off_t offset, int whence);
+extern int fs_close(int fd);
+
+uintptr_t sys_write(int fd, const void *buf, size_t count) {
+	uintptr_t i = 0;
+	if (fd == 1 || fd == 2) {
+		for(; count > 0; count--) {
+			_putc(((char*)buf)[i]);
+			i++;
+		}
+	}
+	return i;
 }
 
-void sys_exit(int a) {
-    _halt(a);
-}
-
-int sys_brk(int addr) {
-    extern int mm_brk(uint32_t new_brk);
-    return mm_brk(addr);
-}
-
-int sys_write(int fd, void* buf, size_t len) {
-    if (fd == 1 || fd == 2) {
-        char c;
-        // Log("buffer:%s", (char*)buf);// for test
-        for (int i = 0; i < len; i++) {
-            memcpy(&c, buf + i, 1);
-            _putc(c);
-        }
-        return len;
-    }
-
-    if (fd >= 3) {
-        return fs_write(fd, buf, len);
-    }
-    Log("fd <= 0");
-    return -1;
-}
-
-int sys_open(const char* filename) {
-    return fs_open(filename, 0, 0);
-}
-
-int sys_read(int fd, void* buf, size_t len) {
-    return fs_read(fd, buf, len);
-}
-
-int sys_close(int fd) {
-    return fs_close(fd);
-}
-
-int sys_lseek(int fd, off_t offset, int whence) {
-    return fs_lseek(fd, offset, whence);
-}
-
+//处理函数
 _RegSet* do_syscall(_RegSet *r) {
-  uintptr_t a[4];
+
+  uintptr_t a[4], neweax = -1;
   a[0] = SYSCALL_ARG1(r);
   a[1] = SYSCALL_ARG2(r);
   a[2] = SYSCALL_ARG3(r);
   a[3] = SYSCALL_ARG4(r);
 
-  switch (a[0]) {
-  case SYS_none:
-      SYSCALL_ARG1(r) = sys_none();
-      break;
-  case SYS_exit:
-      sys_exit(a[1]);
-      break;
-  case SYS_write:
-      SYSCALL_ARG1(r) = sys_write(a[1], (void*)a[2], a[3]);
-      break;
-  case SYS_brk:
-      SYSCALL_ARG1(r) = sys_brk(a[1]);
-      break;
-  case SYS_open:
-      SYSCALL_ARG1(r) = sys_open((char*)a[1]);
-      break;
-  case SYS_read:
-      SYSCALL_ARG1(r) = sys_read(a[1], (void*)a[2], a[3]);
-      break;
-  case SYS_close:
-      SYSCALL_ARG1(r) = sys_close(a[1]);
-      break;
-  case SYS_lseek:
-      SYSCALL_ARG1(r) = sys_lseek(a[1], a[2], a[3]);
-      break;
-  default:
-      panic("Unhandled syscall ID = %d", a[0]);
+  switch (a[0]) {		
+	case SYS_none:
+		neweax = 1;
+		break;
+
+    case SYS_exit:
+		_halt(a[1]);
+		break;	
+		
+	case SYS_write:
+		//neweax = sys_write(a[1], (void *)a[2], a[3]);
+		neweax = fs_write(a[1], (void *)a[2], a[3]);
+		break;
+
+	case SYS_brk:
+		neweax = 0;
+		break;
+
+	case SYS_read:
+		neweax = fs_read(a[1], (void *)a[2], a[3]);
+		break;
+
+	case SYS_open:
+		neweax = fs_open((char *)a[1], a[2], a[3]);
+		break;
+
+	case SYS_close:
+		neweax = fs_close(a[1]);
+		break;
+
+	case SYS_lseek:
+		neweax = fs_lseek(a[1], a[2], a[3]);
+		break;
+
+    default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
+  SYSCALL_ARG1(r) = neweax;
   return NULL;
 }
